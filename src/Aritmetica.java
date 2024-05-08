15b491c383a43c4d9b18b32d6f4632ec7acce483
ntn-sts <160680697+ntn-sts@users.noreply.github.com>
2024-05-08 20:16:34 -0300

    Add files via upload

---
+++ src/Aritmetica.java
@@ -0,0 +1,17 @@
+public class Aritmetica implements ICalcMedia{
+
+    @Override
+    public double CalculaMedia(double p1, double p2){
+        return (p1+p2)/2;
+    }
+
+    @Override
+    public String Situacao(double media) {
+        if (media > 5)
+            return "APROVADO!";
+        else
+            return "REPROVADO!";
+
+    }
+
+}
